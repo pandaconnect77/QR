86315d8c48ed7c5cb38a48dfffb7d83d79e862e7
Panda Connect <122888167+pandaconnect77@users.noreply.github.com>
2025-08-05 10:42:40 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+
+import Home from './Home';
+
+
+const App = () => {
+  return (
+    <Router>
+      
+      <Routes>
+      <Route path="/" element={ <Home/>} />
+       
+        
+      </Routes>
+      
+    </Router>
+  );
+};
+
+export default App;
